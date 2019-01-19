fc62083989ad1d35b08d382d14c98f9f826be097
JacobRoy <jacobroy8597@gmail.com>
2019-01-20 00:00:10 +0530

    Cloud function working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+firebase.initializeApp(functions.config().firebase);
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
@@ -10,9 +12,22 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 exports.addMessage = functions.https.onCall((data) => {
     const message = data.text;
     console.log(message);
-    return {
-        text: "Welcome "+message+"!"
+    const newId = Math.floor(Math.random()*4000)+1000;
+    // return firebase.database().ref('participants/'+newId).set({
+    //     id: newId,
+    //     name: message
+    // });
+    const postData = {
+        id : newId,
+        name: message
     }
+    const newPostKey = firebase.database().ref().child('participants').push().key;
+    let updates = {};
+    updates['/participants/' + newPostKey]= postData;
+    return firebase.database().ref().update(updates);
+    // return {
+    //     text: "Welcome "+message+"!"
+    // }
   });
 
   export const onAddition = functions.database.ref('/participants/{pId}').onCreate((snapshot,context)=>{
